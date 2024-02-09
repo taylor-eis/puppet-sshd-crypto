5b1be999b0d2a780f01b634168fcae8eba397432
brgerig <brgerig@taylor.edu>
2024-02-09 15:50:28 -0500

    Update init.pp
    Fix syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class sshd_crypto (
-  Boolean $manage_sshd_crypto  = $sshd_crypto::params::manage_sshd_crypto
+  Boolean $manage_sshd_crypto  = $sshd_crypto::params::manage_sshd_crypto,
   String $crypto_policy        = $sshd_crypto::params::crypto_policy
 ) inherits $sshd_crypto::params
 {
