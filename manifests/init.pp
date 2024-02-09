a289caa5edc596a43890ddb3cf9c81516d078b30
brgerig <brgerig@taylor.edu>
2024-02-09 16:03:15 -0500

    Update init.pp
    Remove sshd service definition

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,6 @@ class sshd_crypto (
 ) inherits sshd_crypto::params
 {
   if $manage_sshd_crypto {
-    service { 'sshd' :
-      ensure     => 'running',
-      enable     => true,
-    }
     file { 'etc/crypto-policies/back-ends/opensshserver.config':
       ensure    => file,
       content  => "CRYPTO_POLICY='$crypto_policy'",
