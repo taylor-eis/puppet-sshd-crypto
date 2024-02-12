697f9aaa038d3cd83809f67218b732843379f879
brgerig <brgerig@taylor.edu>
2024-02-12 10:34:23 -0500

    Update init.pp
    Add management of ssh modulus file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 class sshd_crypto (
   Boolean $manage_sshd_crypto  = $sshd_crypto::params::manage_sshd_crypto,
-  String $crypto_policy        = $sshd_crypto::params::crypto_policy
+  Boolean $manage_sshd_moduli  = $sshd_crypto::params::manage_sshd_moduli,
+  String $crypto_policy        = $sshd_crypto::params::crypto_policy,
+  Integer $minimum_modulus     = $sshd_crypto::params::minimum_modulus
 ) inherits sshd_crypto::params
 {
   if $manage_sshd_crypto {
@@ -8,6 +10,12 @@ class sshd_crypto (
       ensure    => file,
       content  => "CRYPTO_POLICY='$crypto_policy'",
       notify    => Service['sshd']
+    }
+  }
+  if $manage_sshd_moduli {
+    exec { 'strong_moduli':
+      command => "awk '\$5 >= $minimum_modulus' /etc/ssh/moduli > /etc/ssh/moduli.safe && mv -f /etc/ssh/moduli.safe /etc/ssh/moduli",
+      onlyif => "[ `awk '\$5 < $minimum_modulus && \$1 != \"\#\"' /etc/ssh/moduli | wc -l` -gt 0 ]"
     }
   }
 }
